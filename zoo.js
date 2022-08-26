eabe8ec240b424008c3b1760f063817d5458b03a
jesusverma <jesus.verma@gmail.com>
2022-08-26 21:30:59 +0530

    updated zoo

--- zoo.js
+++ zoo.js
@@ -1,15 +1,1 @@
-function zoo() {
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-  console.log('going to change here by user1')
-
-  console.log('I changed it already by suer2')
-
-  console.log('for rebase')
-=======
-  console.log('I changed it already by suer2')
->>>>>>> Stashed changes
-=======
-  console.log('I changed it already by suer2')
->>>>>>> Stashed changes
-}
+function zoo() {}
