02d9ae9b445f6631bebce0b068c7468fc97055d5
jesusverma <jesus.verma@gmail.com>
2022-08-26 21:04:40 +0530

    changed log of zoo func

--- zoo.js
+++ zoo.js
@@ -1,5 +1,5 @@
 function zoo() {
-  console.log('going to change here by user1')
+  console.log('going to change here by user1 , changes is alredy done')
 
   console.log('I changed it already by suer2')
 
