ad21aa8ee48f3ae91c84eeee510947d686e1173a
jesusverma <jesus.verma@gmail.com>
2022-08-26 21:58:22 +0530

    3rd log added in welcome func

--- welcome.js
+++ welcome.js
@@ -1,4 +1,5 @@
 function welcome() {
   console.log('🚀 ~ file: welcome.js ~ line 2 ~ welcome ~ welcome')
   console.log('🚀 ~ file: welcome.js ~ line 2 ~ welcome ~ welcome')
+  console.log('🚀 ~ file: welcome.js ~ line 2 ~ welcome ~ welcome')
 }
